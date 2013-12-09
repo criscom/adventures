c0387ef988d1885d50689e615c7cb835328a7b48
Christoph Potzinger <criscom27@gmail.com>
2013-12-09 11:42:36 +0100

    Box-shadow for News, Organisation, Sulmsee-Tri landing page, Stylings for weather landing page

--- templates/block--menu-block--4.tpl.php
+++ templates/block--menu-block--4.tpl.php
@@ -48,15 +48,15 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="menu-icons"></div>
-  <?php print render($title_prefix); ?>
-<?php if ($block->subject): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-<?php endif;?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+    <?php endif;?>
+    <?php print render($title_suffix); ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <div class="<?php print $classes; ?>">
-     <?php print render($content); ?>
-    </div>  
-  </div>
+    <div class="content"<?php print $content_attributes; ?>>
+        <div class="<?php print $classes; ?>">
+        <?php print render($content); ?>
+        </div>
+    </div>
 </div>
